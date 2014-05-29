ca818cce9c9254d4e0c21919064e92b791ae363a
Imran shaikh <imranshaikh818@gmail.com>
2014-05-29 19:46:07 +0530

    Previous and next Code refactored

--- imgview.php
+++ imgview.php
@@ -44,61 +44,34 @@
         height: 1700px !important;
       }
 
-      /*.row {
-        padding: 30px 40px;
-        border-radius: 20px;
-        background: #918A8A;
-        border: 3px solid white;
-        position: relative;
-        box-shadow: 0px 0px 5px #000;
-      }*/
-      .row{
-        background: #918A8A;
-      }
-
       .content-slide {
         padding: 20px;
         color: #fff;
       }
+
       .title {
         font-size: 25px;
         margin-bottom: 10px;
       }
-      .pagination {
-        position: absolute;
-        left: 0;
-        text-align: center;
-        bottom:5px;
-        width: 100%;
-      }
-      .swiper-pagination-switch {
-        display: inline-block;
-        width: 10px;
-        height: 10px;
-        border-radius: 10px;
-        background: #999;
-        box-shadow: 0px 1px 2px #555 inset;
-        margin: 0 3px;
-        cursor: pointer;
-      }
+
       .swiper-active-switch {
         background: #fff;
       }
 
       .container .row #arrows .arrow-left {
-        background: url(img/arrows.png) no-repeat left top;
+        background: url(img/arrow.png) no-repeat left top;
         position: absolute;
-        left: 0px;
-        top: 20%;
+        left: -10px;
+        top: 10% !important;
         margin-top: -15px;
         width: 17px;
         height: 30px;
       }
       .container .row #arrows .arrow-right {
-        background: url(img/arrows.png) no-repeat left bottom;
+        background: url(img/arrow.png) no-repeat left bottom;
         position: absolute;
-        right: 0px;
-        top: 20%;
+        right: -10px;
+        top: 10% !important;
         margin-top: -15px;
         width: 17px;
         height: 30px;
@@ -125,11 +98,20 @@
                     $colorname = $colresult['name'];
                     $colorcode = $colresult['code'];
                     $sizeqry = mysql_query("SELECT `source`,`vouchdt`, `itemcode`, `itemname`, `color`, `itemgrp`, SUM(s5) as s5, SUM(s6) as s6, SUM(s7) as s7, SUM(s8) as s8, SUM(s9) as s9, SUM(s10) as s10, SUM(s11) as s11, SUM(s12) as s12, SUM(soth) as soth, `qty`, `mrp`, `rate`  FROM `transacn` WHERE `itemname` = '$itemname' AND `color` = '$colorcode' $groupcond $datecond GROUP BY itemname ") or die(mysql_error());
-                    while ($result = mysql_fetch_assoc($sizeqry)) {
+                    if(mysql_num_rows($sizeqry) >= 1){
+                      $record = 1;
+                      while ($result = mysql_fetch_assoc($sizeqry)) {
                     ?>
                       <div class="swiper-slide">
                         <div>
-                          <?php echo "<img src='".$allitems['imgpath']."' class='img-responsive'>"; ?>
+                          <?php
+                            $image = $allitems['imgpath'];
+                            if(file_exists($image)){
+                              echo "<img src='".$allitems['imgpath']."' class='img-responsive'>";
+                            }else{
+                              echo "<img src='upload/default.jpg' class='img-responsive'>";
+                            }
+                          ?>
                         </div>
                         <table class="table table-bordered">
                           <tr>
@@ -229,9 +211,15 @@
                           </tr>
                         </table>
                       </div>
-                  <?php
+                    <?php
+                      }
+                    }else{
+                     $record = 0;
                     }
                   }
+                }
+                if(empty($record)){
+                  header("Location: imgstock.php");
                 }
               ?>
             </div>
